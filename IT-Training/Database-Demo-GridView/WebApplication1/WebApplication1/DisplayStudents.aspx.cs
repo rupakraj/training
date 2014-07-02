20a24fbda5e6a59965efeeabbe07cea9ffb66966
rughimire <rughimire@gmail.com>
2014-07-02 18:29:07 +0545

    Database Basic Conectivity and Grid View Basic

---
+++ IT-Training/Database-Demo-GridView/WebApplication1/WebApplication1/DisplayStudents.aspx.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace WebApplication1
+{
+    public partial class DisplayStudents : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection("User ID=sa;Initial Catalog=ITTraingG3;Data Source=RAJ\\SQLEXPRESS;Password=raj;");
+            DataTable dt = new DataTable();
+            conn.Open();
+            SqlDataAdapter sda = new SqlDataAdapter("select * from tblStudent where StuAddress ='ktm' ", conn);
+            sda.Fill(dt);
+            GridView1.DataSource = dt;
+            GridView1.DataBind();
+
+        }
+    }
+}
