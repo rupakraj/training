4879adf635f5c1aec0b635f48dcb9a5c9f111996
rughimire <rughimire@gmail.com>
2014-07-07 22:27:05 +0545

    Insert operation using C#

---
+++ IT-Training/CRUD-Basic/CRUDBasic/CRUDBasic/NewStudent.aspx
@@ -0,0 +1,53 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="NewStudent.aspx.cs" Inherits="CRUDBasic.NewStudent" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    <table>
+        <tr>
+            <td>
+                Name
+            </td>
+            <td>
+                <asp:TextBox ID ="txtName" runat="server" required ="true"></asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                Address
+            </td>
+            <td>
+                <asp:TextBox ID ="txtAddress" runat="server" required ="true"></asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td>
+                RollNo
+            </td>
+            <td>
+                <asp:TextBox TextMode="Number"  ID ="txtRollNo" runat="server" required ="true"></asp:TextBox>
+            </td>
+        </tr>
+        <tr>
+            <td>
+
+            </td>
+            <td>
+                <div style ="float:right;">
+                    <asp:Button ID="btnSave" Text="Save" runat="server" OnClick="btnSave_Click" />
+                </div>
+
+            </td>
+        </tr>
+         
+    </table>
+    </div>
+    </form>
+</body>
+</html>
---
+++ IT-Training/CRUD-Basic/CRUDBasic/CRUDBasic/NewStudent.aspx.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+
+
+namespace CRUDBasic
+{
+    public partial class NewStudent : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnSave_Click(object sender, EventArgs e)
+        {
+            string connString = @"Data Source=RAJ\SQLEXPRESS; User ID=sa; Password=raj; Initial Catalog=ITTraingG3;";
+            SqlConnection conn = new SqlConnection(connString);
+            conn.Open();
+            string sql = "insert into tblStudent ";
+            sql += " ( stuName, stuAddress, RollNo )";
+            sql += @" values('" + txtName.Text.Trim() +
+                @"','" + txtAddress.Text.Trim() +
+                @"','" + txtRollNo.Text.Trim() + @"')";
+
+            //SqlCommand cmd = new SqlCommand(sql, conn);
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = conn;
+            cmd.CommandText = sql;
+            int rows_affected = cmd.ExecuteNonQuery();
+            conn.Close();
+            if (rows_affected == 1)
+            {
+                Response.Write("Saved Successfully");
+            }
+            else
+            {
+                Response.Write("Failed");
+            }
+        }
+    }
+}
